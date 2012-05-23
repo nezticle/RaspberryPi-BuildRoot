250c253c158fb3c6a85337ff88862a8daeb6e36b
Andy Nichols <nezticle@gmail.com>
2012-05-23 22:56:10 +0200

    Make Raspberry Pi toolchain an external toolchain, sorry x64 only.

--- package/raspberrypi/bootloader/bootloader.mk
+++ package/raspberrypi/bootloader/bootloader.mk
@@ -5,6 +5,7 @@ BOOTLOADER_INSTALL_TARGET = YES
 
 define BOOTLOADER_INSTALL_TARGET_CMDS
 	echo "Pull bootloaders from git"
+	mkdir -p $(TARGET_DIR)/boot
 	cp $(@D)/boot/arm128_start.elf $(TARGET_DIR)/boot/start.elf
 	cp $(@D)/boot/bootcode.bin $(TARGET_DIR)/boot/bootcode.bin
 	cp $(@D)/boot/loader.bin $(TARGET_DIR)/boot/loader.bin
