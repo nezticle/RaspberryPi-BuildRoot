abc3a01d5a319f1eb5da018741ee1ed4e0618937
Andy Nichols <andy.nichols@nokia.com>
2012-07-17 13:48:11 +0200

    Fix issue where uncompressed kernel is not built

--- package/raspberrypi/bootloader/bootloader.mk
+++ package/raspberrypi/bootloader/bootloader.mk
@@ -6,7 +6,15 @@ BOOTLOADER_INSTALL_TARGET = YES
 define BOOTLOADER_INSTALL_TARGET_CMDS
 	echo "Pull bootloaders from git"
 	mkdir -p $(TARGET_DIR)/boot
-	cp $(@D)/boot/arm$(BR2_RASPBERRYPI_RAM_SIZE)_start.elf $(TARGET_DIR)/boot/start.elf
+	$(if $(BR2_RASPBERRYPI_RAM_SIZE_128),
+		cp $(@D)/boot/arm128_start.elf $(TARGET_DIR)/boot/start.elf
+	)
+	$(if $(BR2_RASPBERRYPI_RAM_SIZE_192),
+		cp $(@D)/boot/arm192_start.elf $(TARGET_DIR)/boot/start.elf
+	)
+	$(if $(BR2_RASPBERRYPI_RAM_SIZE_224),
+		cp $(@D)/boot/arm224_start.elf $(TARGET_DIR)/boot/start.elf
+	)
 	cp $(@D)/boot/bootcode.bin $(TARGET_DIR)/boot/bootcode.bin
 	cp $(@D)/boot/loader.bin $(TARGET_DIR)/boot/loader.bin
 endef
