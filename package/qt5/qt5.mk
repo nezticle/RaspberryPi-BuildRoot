194a6d6b2ef4b9be3184bea6f0ca68c661daf42f
Andy Nichols <andy.nichols@nokia.com>
2012-08-16 16:07:46 +0200

    Add qtjsbackend module package to buildroot

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -1,2 +1,3 @@
 include package/qt5/qtbase/qtbase.mk
 include package/qt5/qtxmlpatterns/qtxmlpatterns.mk
+include package/qt5/qtjsbackend/qtjsbackend.mk
---
+++ package/qt5/qtjsbackend/qtjsbackend.mk
@@ -0,0 +1,29 @@
+QTJSBACKEND_VERSION = master
+QTJSBACKEND_SITE = git://gitorious.org/qt/qtjsbackend.git
+QTJSBACKEND_SITE_METHOD = git
+QTJSBACKEND_DEPENDENCIES = qtbase
+QTJSBACKEND_INSTALL_STAGING = YES
+
+define QTJSBACKEND_CONFIGURE_CMDS
+	-[ -f $(@D)/Makefile ] && $(MAKE) -C $(@D) distclean
+	#run qmake
+	(cd $(@D) && $(HOST_DIR)/usr/bin/qmake )
+endef
+
+define QTJSBACKEND_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QTJSBACKEND_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) install
+endef
+
+define QTJSBACKEND_INSTALL_TARGET_CMDS
+	cp -dpf $(STAGING_DIR)/usr/lib/libQtV8*.so.* $(TARGET_DIR)/usr/lib
+endef
+
+define QTJSBACKEND_UNINSTALL_TARGET_CMDS
+	-rm $(TARGET_DIR)/usr/lib/libQtV8*.so.*
+endef
+
+$(eval $(call GENTARGETS))
