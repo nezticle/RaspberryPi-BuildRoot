845c9f8de3f96a47afed72aff04daf24da7404bf
Andy Nichols <andy.nichols@nokia.com>
2012-08-16 15:38:38 +0200

    Add qtxmlpatterns module package to buildroot

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -1,1 +1,2 @@
 include package/qt5/qtbase/qtbase.mk
+include package/qt5/qtxmlpatterns/qtxmlpatterns.mk
---
+++ package/qt5/qtxmlpatterns/qtxmlpatterns.mk
@@ -0,0 +1,29 @@
+QTXMLPATTERNS_VERSION = master
+QTXMLPATTERNS_SITE = git://gitorious.org/qt/qtxmlpatterns.git
+QTXMLPATTERNS_SITE_METHOD = git
+QTXMLPATTERNS_DEPENDENCIES = qtbase
+QTXMLPATTERNS_INSTALL_STAGING = YES
+
+define QTXMLPATTERNS_CONFIGURE_CMDS
+	-[ -f $(@D)/Makefile ] && $(MAKE) -C $(@D) distclean
+	#run qmake
+	(cd $(@D) && $(HOST_DIR)/usr/bin/qmake )
+endef
+
+define QTXMLPATTERNS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QTXMLPATTERNS_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) install
+endef
+
+define QTXMLPATTERNS_INSTALL_TARGET_CMDS
+	cp -dpf $(STAGING_DIR)/usr/lib/QtXmlPatterns*.so.* $(TARGET_DIR)/usr/lib
+endef
+
+define QTXMLPATTERNS_UNINSTALL_TARGET_CMDS
+	-rm $(TARGET_DIR)/usr/lib/QtXmlPatterns*.so.*
+endef
+
+$(eval $(call GENTARGETS))
