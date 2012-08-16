db3ab529ee96e1a1c7da4253958777b27dfb7748
Andy Nichols <andy.nichols@nokia.com>
2012-08-16 16:59:47 +0200

    Add qtmultimedia module package to buildroot

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -2,3 +2,4 @@ include package/qt5/qtbase/qtbase.mk
 include package/qt5/qtxmlpatterns/qtxmlpatterns.mk
 include package/qt5/qtjsbackend/qtjsbackend.mk
 include package/qt5/qtdeclarative/qtdeclarative.mk
+include package/qt5/qtmultimedia/qtmultimedia.mk
