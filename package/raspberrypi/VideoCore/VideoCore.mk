4c9a13463f141a977c3034909d1b8d73d8126d7e
Andy Nichols <nezticle@gmail.com>
2012-08-23 17:10:47 +0200

    Update to latest Raspberry Pi Kernel and Firmware

--- package/raspberrypi/VideoCore/VideoCore.mk
+++ package/raspberrypi/VideoCore/VideoCore.mk
@@ -1,4 +1,4 @@
-VIDEOCORE_VERSION = 7680bb3
+VIDEOCORE_VERSION = c42f212
 VIDEOCORE_SITE = http://bsquask.com/downloads/firmware
 VIDEOCORE_SOURCE = raspberrypi-firmware-$(VIDEOCORE_VERSION).tar.gz
 VIDEOCORE_INSTALL_STAGING = YES
