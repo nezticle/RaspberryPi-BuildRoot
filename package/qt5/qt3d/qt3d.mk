b175f16c08ae7d6cb5cea1088bef057150f8cd8d
Andy Nichols <nezticle@gmail.com>
2012-08-23 17:53:00 +0200

    Add Qt 5 module Qt3D

---
+++ package/qt5/qt3d/qt3d.mk
@@ -0,0 +1,31 @@
+QT3D_VERSION = c7182244dd2d0b1d0d089fd71081f9e8ec2fbf4c
+QT3D_SITE = git://gitorious.org/qt/qt3d.git
+QT3D_SITE_METHOD = git
+QT3D_DEPENDENCIES = qtbase qtxmlpatterns qtjsbackend qtdeclarative
+QT3D_INSTALL_STAGING = YES
+
+define QT3D_CONFIGURE_CMDS
+	-[ -f $(@D)/Makefile ] && $(MAKE) -C $(@D) distclean
+	#run qmake
+	(cd $(@D) && $(HOST_DIR)/usr/bin/qmake )
+endef
+
+define QT3D_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QT3D_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) install
+endef
+
+define QT3D_INSTALL_TARGET_CMDS
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt3D*.so.* $(TARGET_DIR)/usr/lib
+	cp -dpfr $(STAGING_DIR)/usr/imports/Qt3D $(TARGET_DIR)/usr/imports
+endef
+
+define QT3D_UNINSTALL_TARGET_CMDS
+	-rm $(TARGET_DIR)/usr/lib/libQt3D*.so.*
+	-rm -r $(TARGET_DIR)/usr/imports/Qt3D
+endef
+
+$(eval $(call GENTARGETS))
