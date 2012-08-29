b330a344626561817c78eedd91da3b0e7b948255
Andy Nichols <andy.nichols@nokia.com>
2012-08-29 17:55:55 +0200

    Package omxplayer for multimedia playback support

--- package/raspberrypi/raspberrypi.mk
+++ package/raspberrypi/raspberrypi.mk
@@ -1,3 +1,4 @@
 include package/raspberrypi/bootloader/bootloader.mk
 include package/raspberrypi/mkimage/mkimage.mk
 include package/raspberrypi/VideoCore/VideoCore.mk
+include package/raspberrypi/omxplayer/omxplayer.mk
