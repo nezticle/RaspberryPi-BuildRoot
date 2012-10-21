9343b0867c5231b22fdc77a4ce736cc2c32494ae
Andy Nichols <andy.nichols@digia.com>
2012-10-21 21:48:36 +0200

    Amount of GPU ram split now configurable
    Use BR2_RASPBERRYPI_GPU_RAM_SIZE to set the amount
    of RAM you want dedicated to the GPU.  Has to be
    greater than 16.

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -3,12 +3,6 @@ TARGETDIR=$1
 # mkpasswd, from the 'whois' package in Debian/Ubuntu.
 sed -i 's%^root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 
-# Generate boot config files
-echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > $TARGETDIR/boot/cmdline.txt
-echo "disable_overscan=1" > $TARGETDIR/boot/config.txt
-echo "arm_freq=800" >> $TARGETDIR/boot/config.txt
-echo "gpu_mem=128" >> $TARGETDIR/boot/config.txt
-
 # Point /bin/sh to /bin/bash
 ln -T -s /bin/bash $TARGETDIR/bin/sh 
 
--- package/raspberrypi/bootloader/bootloader.mk
+++ package/raspberrypi/bootloader/bootloader.mk
@@ -10,6 +10,11 @@ define BOOTLOADER_INSTALL_TARGET_CMDS
 	cp $(@D)/boot/bootcode.bin $(TARGET_DIR)/boot/bootcode.bin
 	cp $(@D)/boot/fixup.dat $(TARGET_DIR)/boot/fixup.dat
 	cp $(@D)/boot/fixup_cd.dat $(TARGET_DIR)/boot/fixup_cd.dat
+	# Generate boot config files
+	echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > $(TARGET_DIR)/boot/cmdline.txt
+	echo "disable_overscan=1" > $(TARGET_DIR)/boot/config.txt
+	echo "arm_freq=800" >> $(TARGET_DIR)/boot/config.txt
+	echo "gpu_mem="$(BR2_RASPBERRYPI_GPU_RAM_SIZE) >> $(TARGET_DIR)/boot/config.txt
 endef
 
 $(eval $(generic-package))
