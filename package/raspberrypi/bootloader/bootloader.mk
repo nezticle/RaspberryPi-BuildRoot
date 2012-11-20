5912e5b8c5d76539274b90d58120c0247728d559
Andy Nichols <andy.nichols@digia.com>
2012-11-20 14:57:50 +0100

    Add a respectable overclocking profile by default

--- package/raspberrypi/bootloader/bootloader.mk
+++ package/raspberrypi/bootloader/bootloader.mk
@@ -13,7 +13,10 @@ define BOOTLOADER_INSTALL_TARGET_CMDS
 	# Generate boot config files
 	echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > $(TARGET_DIR)/boot/cmdline.txt
 	echo "disable_overscan=1" > $(TARGET_DIR)/boot/config.txt
-	echo "arm_freq=800" >> $(TARGET_DIR)/boot/config.txt
+	echo "arm_freq=1000" >> $(TARGET_DIR)/boot/config.txt
+	echo "core_freq=500" >> $(TARGET_DIR)/boot/config.txt
+	echo "sdram_freq=500" >> $(TARGET_DIR)/boot/config.txt
+	echo "over_voltage=6" >> $(TARGET_DIR)/boot/config.txt
 	echo "gpu_mem="$(BR2_RASPBERRYPI_GPU_RAM_SIZE) >> $(TARGET_DIR)/boot/config.txt
 endef
 
