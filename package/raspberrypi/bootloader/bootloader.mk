39d0be18adcbf94307b334cc7bc53ad1d9193e50
Andy Nichols <andy.nichols@digia.com>
2012-12-23 16:12:27 +0100

    Simplify how bootloader is aquired
    Also change default framebuffer depth to 24bits

--- package/raspberrypi/bootloader/bootloader.mk
+++ package/raspberrypi/bootloader/bootloader.mk
@@ -1,16 +1,17 @@
 BOOTLOADER_VERSION = 7beaaff
 BOOTLOADER_SITE = http://bsquask.com/downloads/firmware
-BOOTLOADER_SOURCE = raspberrypi-firmware-$(BOOTLOADER_VERSION).tar.gz
+BOOTLOADER_SOURCE = raspberrypi-bootloader-$(BOOTLOADER_VERSION).tar.gz
 BOOTLOADER_INSTALL_TARGET = YES
 
 define BOOTLOADER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/boot
-	cp $(@D)/boot/start.elf $(TARGET_DIR)/boot/start.elf
-	cp $(@D)/boot/bootcode.bin $(TARGET_DIR)/boot/bootcode.bin
-	cp $(@D)/boot/fixup.dat $(TARGET_DIR)/boot/fixup.dat
+	cp $(@D)/start.elf $(TARGET_DIR)/boot/start.elf
+	cp $(@D)/bootcode.bin $(TARGET_DIR)/boot/bootcode.bin
+	cp $(@D)/fixup.dat $(TARGET_DIR)/boot/fixup.dat
 	# Generate boot config files
 	echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > $(TARGET_DIR)/boot/cmdline.txt
 	echo "disable_overscan=1" > $(TARGET_DIR)/boot/config.txt
+	echo "framebuffer_depth=24" >> $(TARGET_DIR)/boot/config.txt
 	echo "arm_freq=800" >> $(TARGET_DIR)/boot/config.txt
 	echo "gpu_mem="$(BR2_RASPBERRYPI_GPU_RAM_SIZE) >> $(TARGET_DIR)/boot/config.txt
 endef
