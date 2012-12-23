6823c82a8a7ca58a58bcbc11e1e06cfbfa09e3bb
Andy Nichols <andy.nichols@digia.com>
2012-12-23 17:41:55 +0100

    Build VideoCore libraries ourselves

--- package/raspberrypi/VideoCore/VideoCore.mk
+++ package/raspberrypi/VideoCore/VideoCore.mk
@@ -1,22 +1,24 @@
-VIDEOCORE_VERSION = 7beaaff
-VIDEOCORE_SITE = http://bsquask.com/downloads/firmware
-VIDEOCORE_SOURCE = raspberrypi-firmware-$(VIDEOCORE_VERSION).tar.gz
+VIDEOCORE_VERSION = 83e1597d57766a4dddcf11960289a6bd49d3d1ce
+VIDEOCORE_SITE = git://github.com/raspberrypi/userland.git
+VIDEOCORE_SITE_METHOD = git
+VIDEOCORE_DEPENDENCIES = udev
 VIDEOCORE_INSTALL_STAGING = YES
 VIDEOCORE_INSTALL_TARGET = YES
+VIDEOCORE_CONF_OPT = -DCMAKE_BUILD_TYPE=Release
+VIDEOCORE_POST_INSTALL_TARGET_HOOKS += VIDEOCORE_CLEANUP_TARGET_INSTALL
 
-define VIDEOCORE_INSTALL_STAGING_CMDS
-	cp -r $(@D)/hardfp/opt $(STAGING_DIR)/
-endef
-
-define VIDEOCORE_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/opt/vc
-	cp -r $(@D)/hardfp/opt/vc/bin $(TARGET_DIR)/opt/vc
-	cp -r $(@D)/hardfp/opt/vc/lib $(TARGET_DIR)/opt/vc
-	cp -r $(@D)/hardfp/opt/vc/sbin $(TARGET_DIR)/opt/vc
+define VIDEOCORE_CLEANUP_TARGET_INSTALL
+	# development files are installed to image which need to be removed
+	rm -r $(TARGET_DIR)/opt/vc/include
+	rm -r $(TARGET_DIR)/opt/vc/src
+	rm -r $(TARGET_DIR)/opt/vc/share
+	rm $(TARGET_DIR)/opt/vc/lib/*.a
+	# vcfile daemon needs to be renamed 
+	#mv $(TARGET_DIR)/etc/init.d/vcfiled $(TARGET_DIR)/etc/init.d/S31vcfiled
+	# remove vcfile daemon because it's not compatible (and not used)
+	rm $(TARGET_DIR)/etc/init.d/vcfiled
 	# add /opt/vc/lib to default library path
 	echo /opt/vc/lib >> $(TARGET_DIR)/etc/ld.so.conf
-	# add symlink for compatibility
-	ln -T -s ld-linux.so.3 $(TARGET_DIR)/lib/ld-linux-armhf.so.3
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
